396e4508cbf36cf00ec10c277b1694eff3cf4c68
kdoval <kdoval@uwaterloo.ca>
2024-12-18 09:43:16 +0530

    Properly Configured gh-pages Build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PortfolioWebsiteV3",
+  base: "/",
   css: {
     modules: {
       localsConvention: "camelCase",
